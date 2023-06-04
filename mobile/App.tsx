41ce28d0f9748e1c02ed5b1dab3adfab5f8832b5
thegabrielrodrigues <gabrielrofreitas.dev@gmail.com>
2023-06-04 14:37:58 -0300

    criação da status-bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,9 @@
+import { StatusBar } from 'react-native';
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" />
+    </>
+  );
 }
